3bc29048f085d526514b52a0823c37fe9af35a54
Hannes Oswald <hannes.oswald97@gmail.com>
2022-05-25 18:13:43 +0200

    Put code of master and worker into their own clas

---
+++ MasterProgram.cs
@@ -0,0 +1,46 @@
+﻿using Histogram_Sequential;
+using MPI;
+
+namespace Histogram_MPI
+{
+    internal class MasterProgram
+    {
+        public static void Run(string[] args, Intracommunicator comm)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please specify file to analyze!");
+                return;
+            }
+
+            // read the file
+            // split file into list of n lists, where n is the comm.size - 1
+            string filename = args[0];
+            Console.WriteLine($"Analyzing: {filename}");
+
+            string text = File.ReadAllText(filename);
+
+            string[] parts = HelperFunctions.SplitIntoChunks(text, comm.Size);
+
+            for (int i = 1; i < comm.Size; i++)
+            {
+                comm.Send(parts[i], i, 1);
+                Console.WriteLine($"Sent text part to process {i}.");
+                //Console.WriteLine($"Sample: {parts[i].Substring(0, 30)}...");
+            }
+
+            HistogramDisplay histogramDisplay = new HistogramDisplay();
+
+            Result result = new(new Dictionary<char, int>(), new Dictionary<string, int>());
+            for (int i = 1; i < comm.Size; i++)
+            {
+                Result newResult = comm.Receive<Result>(i, 0);
+                result.combineResults(newResult);
+                Console.WriteLine($"Sent text part to process {i}.");
+            }
+
+            histogramDisplay.Display(result);
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using Histogram_Sequential;
+﻿using Histogram_MPI;
 using MPI;
 
 
@@ -11,61 +11,13 @@ class Program
 
             Intracommunicator comm = Communicator.world;
 
-            // read the file
-            // split file into list of n lists, where n is the comm.size - 1
-
             if (comm.Rank == 0)
             {
-                if (args.Length == 0)
-                {
-                    Console.WriteLine("Please specify file to analyze!");
-                    return;
-                }
-                string filename = args[0];
-                Console.WriteLine($"Analyzing: {filename}");
-
-                string text = File.ReadAllText(filename);
-
-                string[] parts = HelperFunctions.SplitIntoChunks(text, comm.Size);
-
-                for (int i = 1; i < comm.Size; i++)
-                {
-                    comm.Send(parts[i], i, 1);
-                    Console.WriteLine($"Sent text part to process {i}.");
-                    //Console.WriteLine($"Sample: {parts[i].Substring(0, 30)}...");
-                }
-
-                HistogramDisplay histogramDisplay = new HistogramDisplay();
-
-                Result result = new(new Dictionary<char, int>(), new Dictionary<string, int>());
-                for (int i = 1; i < comm.Size; i++)
-                {
-                    Result newResult = comm.Receive<Result>(i, 0);
-                    result.combineResults(newResult);
-                    Console.WriteLine($"Sent text part to process {i}.");
-                }
-
-                histogramDisplay.Display(result);
-
+                MasterProgram.Run(args, comm);
             }
-
             else
             {
-                if (args.Length == 0)
-                {
-                    return;
-                }
-                Console.WriteLine($"Process {comm.Rank} started.");
-                string text = "";
-                comm.Receive(0, 1, out text);
-                Console.WriteLine($"Process {comm.Rank} received it´s text part.");
-
-                ITextCounter counter = new TextCounter();
-                Result result = counter.Count(text);
-                Console.WriteLine($"Process {comm.Rank} has {result.CharacterCounts.Count()} character counts");
-                Console.WriteLine($"Process {comm.Rank} has {result.WordCounts.Count()} word counts");
-
-                comm.Send(result, 0, 0);
+                WorkerProgram.Run(args, comm);
             }
         }
     }
