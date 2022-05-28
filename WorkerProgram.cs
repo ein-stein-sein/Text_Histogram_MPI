6ecf2da1f665d85b39088b79640c8275aa80f58c
Hannes Oswald <hannes.oswald97@gmail.com>
2022-05-28 10:04:40 +0200

    Refactoring

--- WorkerProgram.cs
+++ WorkerProgram.cs
@@ -12,14 +12,13 @@ namespace Histogram_MPI
                 return;
             }
             Console.WriteLine($"Process {comm.Rank} started.");
-            string text = "";
-            comm.Receive(0, 1, out text);
+            comm.Receive(0, 1, out string text);
             Console.WriteLine($"Process {comm.Rank} received it´s text part.");
 
             ITextCounter counter = new TextCounter();
             Result result = counter.Count(text);
-            Console.WriteLine($"Process {comm.Rank} has {result.CharacterCounts.Count()} character counts");
-            Console.WriteLine($"Process {comm.Rank} has {result.WordCounts.Count()} word counts");
+            Console.WriteLine($"Process {comm.Rank} has {result.CharacterCounts.Count} character counts");
+            Console.WriteLine($"Process {comm.Rank} has {result.WordCounts.Count} word counts");
 
             comm.Send(result, 0, 0);
         }
