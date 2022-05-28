b1e2300cc5732a7c05916c97d3993d47ee352307
Hannes Oswald <hannes.oswald97@gmail.com>
2022-05-28 10:24:10 +0200

    fix missing text part bug

--- MasterProgram.cs
+++ MasterProgram.cs
@@ -12,7 +12,7 @@ namespace Histogram_MPI
                 Console.WriteLine("Please specify file to analyze!");
                 return;
             }
-
+            double wtime = Unsafe.MPI_Wtime();
             // read the file
             // split file into list of n lists, where n is the comm.size - 1
             string filename = args[0];
@@ -20,25 +20,26 @@ namespace Histogram_MPI
 
             string text = File.ReadAllText(filename);
 
-            string[] parts = HelperFunctions.SplitIntoChunks(text, comm.Size);
+            int workerCount = comm.Size - 1;
+            string[] parts = HelperFunctions.SplitIntoChunks(text, workerCount);
 
-            for (int i = 1; i < comm.Size; i++)
+            for (int workerProcess = 1; workerProcess <= workerCount; workerProcess++)
             {
-                comm.Send(parts[i], i, 1);
-                Console.WriteLine($"Sent text part to process {i}.");
+                comm.Send(parts[workerProcess - 1], workerProcess, 1);
+                Console.WriteLine($"Sent text part {workerProcess - 1} to process {workerProcess}.");
                 //Console.WriteLine($"Sample: {parts[i].Substring(0, 30)}...");
             }
 
             HistogramDisplay histogramDisplay = new HistogramDisplay();
 
             Result result = new(new Dictionary<char, int>(), new Dictionary<string, int>());
-            for (int i = 1; i < comm.Size; i++)
+            for (int workerProcess = 1; workerProcess <= workerCount; workerProcess++)
             {
-                Result newResult = comm.Receive<Result>(i, 0);
+                Result newResult = comm.Receive<Result>(workerProcess, 0);
                 result.combineResults(newResult);
-                Console.WriteLine($"Sent text part to process {i}.");
             }
-
+            double wtime2 = Unsafe.MPI_Wtime();
+            Console.WriteLine($"Took {wtime2 - wtime} seconds");
             histogramDisplay.Display(result);
 
         }
